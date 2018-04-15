e5ea290a513c972447c7bc747f8f77bf1da40e4f
Hans van den Akker <hans@bizboard.nl>
2018-04-15 22:49:54 +0200

    .

--- precommit.sh
+++ precommit.sh
@@ -2,8 +2,7 @@
 # pre-commit
 # Bizboard Development Workflow
 if ! type "eslint" > /dev/null; then
-  # install foobar here
-  echo "please install eslint"
-else
-  eslint .
+  sudo npm install -g eslint
 fi
+
+eslint .
